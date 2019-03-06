90b20ff047bd0f63342db565c422ffba5960f18b
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-06 14:22:40 -0800

    fixed eslint errors

--- src/action/simpsons.js
+++ src/action/simpsons.js
@@ -4,4 +4,4 @@ export const FETCH_QUOTE = 'FETCH_QUOTE';
 export const fetchQuote = () => ({
   type: FETCH_QUOTE,
   payload: getQuote()
-})
+});
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,5 +4,5 @@ import SimpsonsQuote from '../../containers/SimpsonsQuote';
 export default function App() {
   return (
     <SimpsonsQuote />
-  )
+  );
 }
